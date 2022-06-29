ccc19a930ae2d0c906d122faa69a5c8c791bfc37
Maksimilian Rebrii <maksim.rebriy@gmail.com>
2022-06-29 19:59:23 +0300

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -1,5 +1,8 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,40 +12,49 @@ import java.util.UUID;
 
 @RestController
 public class ResolutionController {
-	private final ResolutionRepository resolutions;
+    private final ResolutionRepository resolutions;
 
-	public ResolutionController(ResolutionRepository resolutions) {
-		this.resolutions = resolutions;
-	}
+    public ResolutionController(ResolutionRepository resolutions) {
+        this.resolutions = resolutions;
+    }
 
-	@GetMapping("/resolutions")
-	public Iterable<Resolution> read() {
-		return this.resolutions.findAll();
-	}
+    @GetMapping("/resolutions")
+    @PreAuthorize("hasAuthority('READ')")
+    @PostFilter("@post.filter(#root)")
+    public Iterable<Resolution> read() {
+        return this.resolutions.findAll();
+    }
 
-	@GetMapping("/resolution/{id}")
-	public Optional<Resolution> read(@PathVariable("id") UUID id) {
-		return this.resolutions.findById(id);
-	}
+    @GetMapping("/resolution/{id}")
+    @PreAuthorize("hasAuthority('resolution:read')")
+    @PostAuthorize("@post.authorize(#root)")
+    public Optional<Resolution> read(@PathVariable("id") UUID id) {
+        return this.resolutions.findById(id);
+    }
 
-	@PostMapping("/resolution")
-	public Resolution make(@CurrentSecurityContext(expression = "authentication.name") String owner,
-						   @RequestBody String text) {
-		Resolution resolution = new Resolution(text, owner);
-		return this.resolutions.save(resolution);
-	}
+    @PostMapping("/resolution")
+    @PreAuthorize("hasAuthority('resolution:write')")
+    public Resolution make(@CurrentSecurityContext(expression = "authentication.name") String owner,
+                           @RequestBody String text) {
+        Resolution resolution = new Resolution(text, owner);
+        return this.resolutions.save(resolution);
+    }
 
-	@PutMapping(path="/resolution/{id}/revise")
-	@Transactional
-	public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
-		this.resolutions.revise(id, text);
-		return read(id);
-	}
+    @PutMapping(path = "/resolution/{id}/revise")
+    @PreAuthorize("hasAuthority('resolution:write')")
+    @PostAuthorize("@post.authorize(#root)")
+    @Transactional
+    public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
+        this.resolutions.revise(id, text);
+        return read(id);
+    }
 
-	@PutMapping("/resolution/{id}/complete")
-	@Transactional
-	public Optional<Resolution> complete(@PathVariable("id") UUID id) {
-		this.resolutions.complete(id);
-		return read(id);
-	}
+    @PutMapping("/resolution/{id}/complete")
+    @PreAuthorize("hasAuthority('resolution:write')")
+    @PostAuthorize("@post.authorize(#root)")
+    @Transactional
+    public Optional<Resolution> complete(@PathVariable("id") UUID id) {
+        this.resolutions.complete(id);
+        return read(id);
+    }
 }
