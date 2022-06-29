114854721ec57d17ea11aa74f808dec7e47978f9
Maksimilian Rebrii <maksim.rebriy@gmail.com>
2022-06-29 18:26:12 +0300

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -3,14 +3,14 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
 	private final ResolutionRepository resolutions;
+	private final UserRepository userRepository;
 
-	public ResolutionInitializer(ResolutionRepository resolutions) {
+	public ResolutionInitializer(ResolutionRepository resolutions, UserRepository userRepository) {
 		this.resolutions = resolutions;
+		this.userRepository = userRepository;
 	}
 
 	@Override
@@ -18,5 +18,21 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 		this.resolutions.save(new Resolution("Read War and Peace", "user"));
 		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
 		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+
+		User user = new User("user",
+				"{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		user.grantAuthority("resolution:read");
+		user.grantAuthority("resolution:write");
+		this.userRepository.save(user);
+
+		User hashRead = new User("hashread",
+				"{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		hashRead.grantAuthority("resolution:read");
+		this.userRepository.save(hashRead);
+
+		User hashWrite = new User("hashwrite",
+				"{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		user.grantAuthority("resolution:write");
+		this.userRepository.save(hashWrite);
 	}
 }
